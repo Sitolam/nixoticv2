a73c856889f43f909becf0184ce4e48359e56906
sitolam <otis.lammertyn@gmail.com>
2024-04-05 14:13:53 +0200

    added nvidia-prime offload

--- hosts/nixotic/default.nix
+++ hosts/nixotic/default.nix
@@ -8,13 +8,14 @@
   imports = [
     ./hardware-configuration.nix
     ./powersave.nix
+    ./nvidia.nix
   ];
 
-#  age.secrets.spotify = {
-#    file = "${self}/secrets/spotify.age";
-#    owner = "mihai";
-#    group = "users";
-#  };
+  #  age.secrets.spotify = {
+  #    file = "${self}/secrets/spotify.age";
+  #    owner = "mihai";
+  #    group = "users";
+  #  };
 
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_cachyos;
   environment.systemPackages = [pkgs.scx];
