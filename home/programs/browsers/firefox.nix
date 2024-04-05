2c3609d637d7c5ae5ebd0dee4d69207c068f8905
sitolam <otis.lammertyn@gmail.com>
2024-04-05 20:54:48 +0200

    added betterfox

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -1,6 +1,30 @@
 {
+  config,
+  inputs,
+  pkgs,
+  self,
+  lib,
+  ...
+}: let
+  extramaker = _profileName: {
+    extraConfig = lib.readFile (toString "${self}/home/programs/browsers/${_profileName}.js");
+  };
+
+  firefoxExtensions = pkgs;
+in {
   programs.firefox = {
     enable = true;
-    profiles.sitolam = {};
+
+    profiles = {
+      betterfox =
+        extramaker "betterfox"
+        // {
+          isDefault = true;
+          extensions = with firefoxExtensions; [
+            nur.repos.rycee.firefox-addons.ublock-origin
+          ];
+          # someOption = "value";
+        };
+    };
   };
 }
